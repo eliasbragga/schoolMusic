068c3c40f225ad4ba87e5af5939460ebd303312a
AntonioALino <antonioaplino@gmail.com>
2024-04-15 18:16:01 -0300

    Project started.

---
+++ schoolMusic-api/src/main/java/br/com/linoandelias/schoolMusicAPI/SchoolMusicApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.linoandelias.schoolMusicAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolMusicApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolMusicApiApplication.class, args);
+	}
+
+}

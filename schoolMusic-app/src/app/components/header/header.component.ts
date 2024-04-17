851eee59aa28696355531436365b4387ad1d02c5
victorlimas <victorlimasantos99@hotmail.com>
2024-04-17 17:36:21 -0300

    Subindo atualização do header e fivicon

--- schoolMusic-app/src/app/components/header/header.component.ts
+++ schoolMusic-app/src/app/components/header/header.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
+  imports: [NgFor]
 })
 export class HeaderComponent {
-
+  links = [
+    { name: 'Comece por aqui' },
+    { name: 'Trilhas' },
+    { name: 'Suporte' }
+  ];
 }

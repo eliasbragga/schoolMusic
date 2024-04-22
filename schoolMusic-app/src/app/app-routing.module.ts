012ee23240926446db0b5e4095d4837344f4e47d
victorlimas <victorlimasantos99@hotmail.com>
2024-04-22 16:13:16 -0300

    Raising skirting boards and your style

--- schoolMusic-app/src/app/app-routing.module.ts
+++ schoolMusic-app/src/app/app-routing.module.ts
@@ -3,9 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './views/login/login.component';
 import { HomeComponent } from './views/home/home.component';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
 ];
--- schoolMusic-app/src/app/app.module.ts
+++ schoolMusic-app/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { RouterModule } from '@angular/router';
 import { InputComponent } from './components/input/input.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
@@ -27,6 +28,7 @@ import { HeaderComponent } from './components/header/header.component';
     InputComponent,
     HomeComponent,
     HeaderComponent,
+    FooterComponent
   ],  
   providers: [],
   bootstrap: [AppComponent]

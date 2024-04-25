c6cb9d30f7eb69768dcba244627bc8f41702988d
victorlimas <victorlimasantos99@hotmail.com>
2024-04-24 21:59:43 -0300

    desbugando

--- schoolMusic-app/src/app/components/card/card.component.ts
+++ schoolMusic-app/src/app/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
-
+  @Input() imageUrl!: string;
 }
--- schoolMusic-app/src/app/views/home/home.component.ts
+++ schoolMusic-app/src/app/views/home/home.component.ts
@@ -1,17 +1,22 @@
 import { Component, OnInit} from '@angular/core';
 import { ContextService } from '../../service/context.service';
 import { CardComponent } from '../../components/card/card.component';
-
+import { NgFor } from '@angular/common';
 
 @Component({
   standalone: true, 
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
-  imports: [CardComponent]
+  imports: [CardComponent,NgFor]
 })  
 export class HomeComponent{
   numeroAtual?: number;
+  images = [
+    { url: '../../../assets/bateria.png' },
+    { url: '../../../assets/piano.png' },
+    { url: '../../../assets/violao.jpg' }
+  ];
   constructor(
     private contextService: ContextService
   ) { }

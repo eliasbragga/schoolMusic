0729d598765454e80306b6b92f2b418b1ebf0c02
elias.alves <elias.alves@az.com.br>
2024-04-25 06:18:41 -0400

    [FEATURE]: adding style in card-component

--- schoolMusic-app/src/app/app.component.ts
+++ schoolMusic-app/src/app/app.component.ts
@@ -16,6 +16,7 @@ export class AppComponent  implements OnInit {
     }
 
   ngOnInit(): void {
+    this.contextService.redirectRoute('inicio')
     this.loggedIn = sessionStorage.getItem("token")
   }
  
